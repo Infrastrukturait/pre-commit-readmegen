adbb0b6f41cf887a51b869f1a186cde55009eb55
Rafał Masiarek <rafal@masiarek.pl>
2022-11-04 19:16:20 +0100

    add GIT_DIR variable

--- hooks/readmegen.sh
+++ hooks/readmegen.sh
@@ -3,24 +3,25 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin
 
 set -eo pipefail
 
-if [ ! -f ./README.json ]; then 
+if [ ! -f "${GIT_DIR}/README.json" ]; then 
     echo "README.json with variables does not exist."
-    exit 1
+    exit 6
 fi
 
 GOMPLATE_BIN=`which gomplate`
 
 if [ -z "$GOMPLATE_BIN" ]; then
     echo "gomplate is necessary and not installed"
-    exit 1
+    exit 4
 fi
 
 CURL_BIN=`which curl`
 
 if [ -z $CURL_BIN ]; then
     echo "curl is necessary and not installed"
-    exit 1
+    exit 2
 fi
 
-$CURL_BIN -Ls https://raw.githubusercontent.com/Infrastrukturait/READMEgen/main/README.md.template | gomplate -d config=./README.json > ./README.md
+$CURL_BIN -Ls https://raw.githubusercontent.com/Infrastrukturait/READMEgen/main/README.md.template | $GOMPLATE_BIN -d config="${GIT_DIR}/README.json" > 
+"${GIT_DIR}/README.md"
 
